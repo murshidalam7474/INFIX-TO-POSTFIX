87fe60475008c4d2f81c22248d17fdb3baef0271
MOHAMMED MURSHID ALAM <83682620+murshidalam7474@users.noreply.github.com>
2024-08-20 19:22:08 +0530

    Update infixtopostfix.c

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -1,129 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
-struct Node
-{
- char data;
- struct Node *next;
-}*top=NULL;
-void push(char x)
-{
- struct Node *t;
- t=(struct Node*)malloc(sizeof(struct Node));
- 
- if(t==NULL)
- printf("stack is full\n");
- else
- {
- t->data=x;
- t->next=top;
- top=t;
- }
- 
-}
-char pop()
-{
- struct Node *t;
- char x=-1;
- 
- if(top==NULL)
- printf("Stack is Empty\n");
- else
- {
- t=top;
- top=top->next;
- x=t->data;
- free(t);
- }
- return x;
-}
-void Display()
-{
- struct Node *p;
- p=top;
- while(p!=NULL)
- {
- printf("%d ",p->data);
- p=p->next;
- }
- printf("\n");
-}
-int isBalanced(char *exp)
-{
- int i;
- 
- for(i=0;exp[i]!='\0';i++)
- {
- if(exp[i]=='(')
- push(exp[i]);
- else if(exp[i]==')')
- {
- if(top==NULL)
- return 0;
- pop();
- }
- }
- if(top==NULL)
- return 1;
- else
- return 0;
-}
-int pre(char x)
-{
- if(x=='+' || x=='-')
- return 1;
- else if(x=='*' || x=='/')
- return 2;
- return 0;
-}
- 
-int isOperand(char x)
-{
- if(x=='+' || x=='-' || x=='*' || x=='/')
- return 0;
- else
- return 1;
- 
-}
- 
-char * InToPost(char *infix)
-{
- int i=0,j=0;
- char *postfix;
- int len=strlen(infix);
- postfix=(char *)malloc((len+2)*sizeof(char));
- 
- while(infix[i]!='\0')
- {
- if(isOperand(infix[i]))
- postfix[j++]=infix[i++];
- else
- {
- if(pre(infix[i])>pre(top->data))
- push(infix[i++]);
- else
- {
- postfix[j++]=pop();
- }
- }
- }
- while(top!=NULL)
- postfix[j++]=pop();
- postfix[j]='\0';
- return postfix;
-}
- 
-int main()
-{
- char *infix="a+b*c-d/e";
- push('#');
- 
- char *postfix=InToPost(infix);
- 
- printf("%s ",postfix);
- 
- 
- return 0;
-}
+class InfixToPostfixConverter:
+    def __init__(self, infix):
+        self.infix = infix
+        self.stack = []
+        self.postfix = []
 
-//infix to postfix
+    def precedence(self, x):
+        if x == '+' or x == '-':
+            return 1
+        elif x == '*' or x == '/':
+            return 2
+        return 0
+
+    def is_operand(self, x):
+        return x not in ['+', '-', '*', '/', '(', ')']
+
+    def convert(self):
+        self.stack.append('#')
+        
+        for ch in self.infix:
+            if self.is_operand(ch):
+                self.postfix.append(ch)
+            elif ch == '(':
+                self.stack.append(ch)
+            elif ch == ')':
+                while self.stack and self.stack[-1] != '(':
+                    self.postfix.append(self.stack.pop())
+                self.stack.pop()  # Remove '(' from the stack
+            else:
+                while (self.stack and self.stack[-1] != '(' and 
+                       self.precedence(ch) <= self.precedence(self.stack[-1])):
+                    self.postfix.append(self.stack.pop())
+                self.stack.append(ch)
+
+        while self.stack and self.stack[-1] != '#':
+            self.postfix.append(self.stack.pop())
+
+        return ''.join(self.postfix)
+
+if __name__ == "__main__":
+    infix = "a+b/c*(d+e*f)-g"
+    converter = InfixToPostfixConverter(infix)
+    postfix = converter.convert()
+    print(postfix)
